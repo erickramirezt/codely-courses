aaf272f19b9ba84748dbdba02e7463a3f59fa8d5
Erick Ramirez <erickramirez.dnbk@gmail.com>
2024-03-26 07:15:54 -0300

    create course form test started

--- react-codely-hexagonal-frontend-course/tests/app/sections/courses/create-course-form.spec.tsx
+++ react-codely-hexagonal-frontend-course/tests/app/sections/courses/create-course-form.spec.tsx
@@ -1,7 +1,22 @@
-import { describe, it } from "vitest";
+import { describe, it } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { LocalStorageCourseRepository } from '../../../../src/modules/courses/infrastructure/local-storage-course-repository'
+import { CoursesContextProvider } from '../../../../src/app/sections/courses/shared/course-context'
+import React from 'react'
+import { CreateCourseForm } from '../../../../src/app/sections/courses/create/create-course-form'
 
 describe('create course form component', () => {
-  it("displays success message when data is correct", async () => {
-    
+  it('displays success message when data is correct', async () => {
+    const repository = new LocalStorageCourseRepository()
+    render(
+      <CoursesContextProvider repository={repository}>
+        <CreateCourseForm />
+      </CoursesContextProvider>
+    )
+
+    const titleInput = screen.getByLabelText(/title/i)
+    userEvent.type(titleInput, "Awesome Hexagonal Architecture")
   })
 })
