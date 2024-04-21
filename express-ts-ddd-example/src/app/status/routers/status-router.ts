0bb0d333a9cb7a182f0686999e371ce3994e96ca
Erick Ramirez <erickramirez.dnbk@gmail.com>
2024-04-21 18:55:44 -0400

    express ts ddd example deleted

--- express-ts-ddd-example/src/app/status/routers/status-router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { getStatusController } from '../controllers/get-status-controller'
-
-const statusRouter = Router()
-
-statusRouter.get('/', getStatusController)
-
-export { statusRouter }
