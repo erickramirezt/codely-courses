6a89dd965776ac8b2bcc4ac1df49f0fc1bac5887
Erick Ramirez <erickramirez.dnbk@gmail.com>
2024-05-01 16:52:39 -0400

    create course request mother params updated

--- express-ts-ddd-example/tests/app/courses/put/put-courses.spec.ts
+++ express-ts-ddd-example/tests/app/courses/put/put-courses.spec.ts
@@ -2,7 +2,6 @@ import test, { APIRequestContext, expect } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
 
 import { coursesApiRoute } from '../../../../src/app/shared/routes/routes'
-// import { InvalidCourseIdError } from '../../../../src/modules/courses/domain/errors/invalid-course-id-error'
 import { CreateCourseRequestMother } from '../../../modules/courses/application/create/create-course-request-mother'
 import { getContext } from '../../shared/api/api-test'
 
@@ -22,16 +21,12 @@ test('put course', async () => {
 	expect(res.status()).toBe(StatusCodes.OK)
 })
 
-// TODO: REFACTOR CREATE COURSE REQUEST MOTHER AND CREATE COURSE REQUEST
-// test('put course with invalid id', async () => {
-// 	const body = CreateCourseRequestMother.createWithInvalidId()
-// 	const res = await api.put(`${coursesApiRoute}/${body.id}`, {
-// 		data: {
-// 			...body
-// 		}
-// 	})
-// 	expect(res.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
-// 	expect(res.body()).toEqual({
-// 		error: InvalidCourseIdError.message
-// 	})
-// })
+test('put course with invalid id', async () => {
+	const body = CreateCourseRequestMother.createWithInvalidId()
+	const res = await api.put(`${coursesApiRoute}/${body.id}`, {
+		data: {
+			...body
+		}
+	})
+	expect(res.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+})
